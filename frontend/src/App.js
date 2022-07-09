76e0c799529ddb3e793d6f87c3302e92e036d26c
Iykescode <chuksikechukwu15@gmail.com>
2022-07-09 16:52:37 +0100

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,28 @@
-import data from "./data";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">amazon</a>
-      </header>
-      <main>
-        <h1>Featured products</h1>
-        <div className="products">
-          {
-            data.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <img src={product.image} alt={product.name} />
-                <p>{product.name}</p>
-                <p>{product.price}</p>
-              </div>
-            ))
-          }
-        </div>
-      </main>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <header className="App-header">
+                    <Link to="/">amazon</Link>
+                </header>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<HomeScreen/>}/>
+                        <Route path="/product/:slug" element={<ProductScreen/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
