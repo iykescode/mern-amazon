9671de999b63489a5c2f43e3fc74ede5acaea441
Iykescode <chuksikechukwu15@gmail.com>
2022-07-09 15:49:59 +0100

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
+import data from "./data";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">amazon</a>
-        <main>
-          Product List
-        </main>
       </header>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+          {
+            data.products.map((product) => (
+              <div className="product" key={product.slug}>
+                <img src={product.image} alt={product.name} />
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+              </div>
+            ))
+          }
+        </div>
+      </main>
     </div>
   );
 }
